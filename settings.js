ca0765d48c8b30fb206ab51983e1396cd2d0157d
KelvNganga <66887366+KelvNganga@users.noreply.github.com>
2022-09-02 22:08:40 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,7 +15,7 @@ global.APIs = {
 
 //Api Key\\
 global.APIKeys = {
-	'https://zenzapis.xyz': 'ZE5ueWhKczU=', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
+	'https://zenzapis.xyz': 'TEhLQzdkTEc=', //credit goes to sanzy yt, if api key expire, buy it and put it here?\\
 }
 
 //customize settings\\
